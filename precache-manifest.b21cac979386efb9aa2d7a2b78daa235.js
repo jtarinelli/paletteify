41eb7becb5a314f396592697bb739974e29458a9
unknown <juliatarinelli4@gmail.com>
2020-07-07 17:09:36 -0400

    Updates

---
+++ precache-manifest.b21cac979386efb9aa2d7a2b78daa235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1efc98b3bfc8632e6b200ef403583c",
+    "url": "/paletteify/index.html"
+  },
+  {
+    "revision": "c68b8d1e6fec97686fbc",
+    "url": "/paletteify/static/css/main.e3bddee0.chunk.css"
+  },
+  {
+    "revision": "dee04bfe5baf5e93e11e",
+    "url": "/paletteify/static/js/2.5a6c299d.chunk.js"
+  },
+  {
+    "revision": "0d69092c1a98f7b51d1c076390db4cf5",
+    "url": "/paletteify/static/js/2.5a6c299d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68b8d1e6fec97686fbc",
+    "url": "/paletteify/static/js/main.a6a59065.chunk.js"
+  },
+  {
+    "revision": "109e6cf625f5dd538f87",
+    "url": "/paletteify/static/js/runtime-main.3471e6ae.js"
+  }
+]);
