89ca5115b7a4869b09d765beba6862fa62f36660
unknown <juliatarinelli4@gmail.com>
2020-07-31 17:25:10 -0400

    Updates

--- precache-manifest.bc9c6f59c0364a5cbfd9a35bc0d31355.js
+++ precache-manifest.bc9c6f59c0364a5cbfd9a35bc0d31355.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0907896109d3fe899cdd77a1b648536b",
+    "revision": "5df6e7725691a42bffabead19cad5eee",
     "url": "/paletteify/index.html"
   },
   {
-    "revision": "38bacacfa2b4ec8160d3",
+    "revision": "743fd4c2d46755233b93",
     "url": "/paletteify/static/css/main.0571d01c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/paletteify/static/js/2.9bbd84c8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "38bacacfa2b4ec8160d3",
-    "url": "/paletteify/static/js/main.ca5dc3c3.chunk.js"
+    "revision": "743fd4c2d46755233b93",
+    "url": "/paletteify/static/js/main.b873105c.chunk.js"
   },
   {
     "revision": "109e6cf625f5dd538f87",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/paletteify/precache-manifest.e9ae8aada3eddae05e609c27e9cf9527.js"
+  "/paletteify/precache-manifest.bc9c6f59c0364a5cbfd9a35bc0d31355.js"
 );
 
 self.addEventListener('message', (event) => {
